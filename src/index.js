1743030c746e49999e6432737b461b7832a54ae4
Dmitry Void <supsad@yahoo.com>
2024-05-18 00:14:23 +0700

    refactoring code

--- src/index.js
+++ src/index.js
@@ -13,35 +13,35 @@ const changeCategories = (currentCategory) => {
   let activeTarget = currentCategory;
 
   categoryButtons.forEach(category => {
-    category.addEventListener('click', (e) => {
-      e.preventDefault();
+    category.addEventListener('click', (ev) => {
+      ev.preventDefault();
 
-      if (activeTarget !== e.currentTarget) {
+      if (activeTarget !== ev.currentTarget) {
         activeTarget.classList.remove('store__categories-item_current');
       }
 
-      activeTarget = e.currentTarget;
+      activeTarget = ev.currentTarget;
       activeTarget.classList.add('store__categories-item_current');
-      void fetchProductByCategory(`${e.target.dataset.category}`);
+      void fetchProductByCategory(`${ev.target.dataset.category}`);
     });
   });
 };
 
-const createProductCard = (product) => {
+const createProductCard = ({name, price, photoUrl}) => {
   const productCard = document.createElement('li');
   productCard.classList.add('store__catalog-item');
 
   productCard.innerHTML = `
   <article class="product-card">
     <div class="product-card__picture-container">
-      <img class="product-card__picture" src="${API_URL}${product.photoUrl}" 
-      alt="${product.name}" width="388" height="261">
+      <img class="product-card__picture" src="${API_URL}${photoUrl}" 
+      alt="${name}" width="388" height="261">
     </div>
     <h3 class="product-card__title">
-      ${product.name}
+      ${name}
     </h3>
     <p class="product-card__price">
-      ${product.price}&nbsp;
+      ${price}&nbsp;
       <span>&#8381;</span>
     </p>
     <button class="button button_purple product-card__buy-button" type="button">Заказать</button>
@@ -60,20 +60,22 @@ const renderProducts = (products) => {
   });
 };
 
+const checkData = (data) => {
+  if (!data.ok) throw new Error(`Ошибка запроса: ${data.status} ${data.statusText}`);
+};
+
 const fetchProductByCategory = async (category) => {
   try {
     const response = await fetch(`${API_URL}/api/products/category/${category}`);
 
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
+    checkData(response);
 
     const products = await response.json();
     console.log('products:', products);
 
     renderProducts(products);
   } catch (e) {
-    console.error(`Ошибка запроса товара: ${e}`);
+    console.error(e);
   }
 };
 
@@ -91,20 +93,20 @@ const setDefaultCategory = (category) => {
   return category;
 };
 
-const openCart = (e) => {
-  e.preventDefault();
+const openCart = (ev) => {
+  ev.preventDefault();
 
   modalOverlay.classList.add('modal-overlay_show');
 
-  if (e.target === e.currentTarget || e.target.closest('.modal-overlay__close-button')) {
+  if (ev.target === ev.currentTarget || ev.target.closest('.modal-overlay__close-button')) {
     modalOverlay.classList.remove('modal-overlay_show');
     modalOverlay.removeEventListener('click', openCart);
   }
 };
 
-const cartHandler = () => {
-  cartButton.addEventListener('click', (e) => {
-    e.preventDefault();
+const cartOpenHandler = () => {
+  cartButton.addEventListener('click', (ev) => {
+    ev.preventDefault();
 
     modalOverlay.classList.add('modal-overlay_show');
     modalOverlay.addEventListener('click', openCart);
@@ -112,7 +114,7 @@ const cartHandler = () => {
 };
 
 const init = () => {
-  cartHandler();
+  cartOpenHandler();
 
   let currentCategory = getCurrentCategory();
   currentCategory ??= setDefaultCategory(currentCategory);
